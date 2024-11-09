f31c79528fa73bab182244f719647966fd8ec56f
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-11-09 16:52:05 +0000

    add new file

--- userauths/forms.py
+++ userauths/forms.py
@@ -12,3 +12,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match.")
--- userauths/views.py
+++ userauths/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.http import HttpResponse
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 def register_view(request):
 
@@ -12,7 +15,7 @@ def register_view(request):
             new_user = form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"Hey {username}, Your account was created successfully.")
-            new_user = authenticate(username=form.cleaned_data['email'],
+            new_user = authenticate(email=form.cleaned_data['email'],
                                     password=form.cleaned_data['password1'],
                                     )
 
@@ -20,12 +23,47 @@ def register_view(request):
             return render(request, 'core/index.html')
 
         else:
-            print("User cannot be registered!!!")
+            form = UserRegisterForm()
 
-
+    
     form = UserRegisterForm()
     context = {
         'form': form,
     }
     return render(request, 'userauths/sign-up.html', context) 
 
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are already logged in')
+        return redirect('core:index')
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            #messages.warning(request, f'User with {email} does not exist')
+            return redirect('userauths:sign-in')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('core:index')
+        else:
+            messages.warning(request, 'Username OR password is incorrect')
+
+    context = {
+
+    }
+
+    return render(request, 'userauths/sign-in.html', context)
+    
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You are now logged out')
+    return redirect('userauths:sign-in')
