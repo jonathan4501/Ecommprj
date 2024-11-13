c983ac2c7e6110aefe8f025ca9683e18c67b4128
Jonathan Edem Koffi Ameko <jonathan.ameko4501@gmail.com>
2024-11-13 15:50:43 +0000

    add new file

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from core.models import Category, Product, ProductImages, Vendor, Wishlist, ProductReview, CartOrder, CartOrderItem, Address
 
-# Register your models here.
+class ProductImagesAdmin(admin.TabularInline):
+    model = ProductImages
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesAdmin]
+    list_display = ('user', 'title', 'product_image', 'price', 'featured', 'product_status' )
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category_image')
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('title', 'vendor_image')
+
+class CartOrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'price', 'paid_status', 'order_date', 'product_status')
+
+class CartOrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'invoice_number', 'item', 'image', 'qty', 'price', 'total')
+
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'review', 'rating')
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'date')
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address', 'status')
+
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(CartOrder, CartOrderAdmin)
+admin.site.register(CartOrderItem, CartOrderItemAdmin)
+admin.site.register(ProductReview, ProductReviewAdmin)
+admin.site.register(Wishlist, WishlistAdmin)
+admin.site.register(Address, AddressAdmin)
+
--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from core.models import Category, Product, ProductImages, Vendor, Wishlist, ProductReview, CartOrder, CartOrderItem, Address
 
 def index(request):
-    return render(request, 'core/index.html')
+    product = Product.objects.all()
+
+    context = {
+        'products': product
+    }
+
+    return render(request, 'core/index.html', context)
